5130b0f8b76084307aa1f8697a7ee9beafea4603
Ömer Düzgün <Cecemaru@hotmail.com>
2022-10-30 02:14:04 +0300

    filter buttonlari onyuz tasarimi tamamlandi main js de active ve tag aramalari yazildi

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,31 @@ softwareAll.forEach((softwareSelect) => {
 });
 
 // =============Software Active Button ========
+
+// =============Filter Category Tag Check ========
+const tagFilters = document.querySelectorAll(".tag-filter .post-tag");
+const postLists = document.querySelectorAll(".post-container .post-listing");
+
+tagFilters.forEach((tagFilter) => {
+  tagFilter.onclick = function () {
+    //active
+    tagFilters.forEach((tagFilter) => {
+      tagFilter.className = "post-tag";
+    });
+    tagFilter.className = "post-tag active";
+
+    //Filter
+    const value = tagFilter.textContent;
+    postLists.forEach((postList) => {
+      postList.style.display = "none";
+      if (
+        postList.getAttribute("data-filter") == value.toLowerCase() ||
+        value == "All"
+      ) {
+        postList.style.display = "block";
+      }
+    });
+  };
+});
+
+// ================ Filter Category Tag Check End ======
