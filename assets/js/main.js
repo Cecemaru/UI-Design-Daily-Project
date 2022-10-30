3b4b34b8154fce6115fa0f7d819d150610e6a749
Ömer Düzgün <Cecemaru@hotmail.com>
2022-10-31 01:44:43 +0300

    header onclick ozelligi ile tekrardan yapildi ve js dosyasinda filter ozellikleri guncellendi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,21 @@
-// ==============Software Active Button js =========
-const softwareAll = document.querySelectorAll(".software-item");
-
-softwareAll.forEach((softwareSelect) => {
-  softwareSelect.addEventListener("click", () => {
-    softwareAll.forEach((newbtn) => newbtn.classList.remove("active"));
-    softwareSelect.classList.add("active");
-  });
-});
+const wrapper = document.querySelector(".wrapper");
+function menuOpen() {
+  wrapper.classList.toggle("show");
+}
 
 // =============Software Active Button ========
+
+const uiSearch = document.querySelector(".search-input");
+uiSearch.addEventListener("keyup", filterUi);
+
+function filterUi() {
+  let filterValue = uiSearch.value.toLowerCase();
+  const posts = document.querySelectorAll(".post-listing").forEach((post) => {
+    post.innerText.toLowerCase().indexOf(filterValue) > -1
+      ? (post.style.display = "")
+      : (post.style.display = "none");
+  });
+}
 
 // =============Filter Category Tag Check ========
 const tagFilters = document.querySelectorAll(".tag-filter .post-tag");
@@ -23,13 +30,15 @@ tagFilters.forEach((tagFilter) => {
     tagFilter.className = "post-tag active";
 
     //Filter
-    const value = tagFilter.textContent;
+    const tagValue = tagFilter.textContent;
     postLists.forEach((postList) => {
+      postList.className = "";
       postList.style.display = "none";
       if (
-        postList.getAttribute("data-filter") == value.toLowerCase() ||
-        value == "All"
+        postList.getAttribute("data-tag-filter") == tagValue.toLowerCase() ||
+        tagValue == "All"
       ) {
+        postList.className = "post-listing select";
         postList.style.display = "block";
       }
     });
@@ -37,3 +46,31 @@ tagFilters.forEach((tagFilter) => {
 });
 
 // ================ Filter Category Tag Check End ======
+
+// ==============Software Active Button js =========
+const softwareAll = document.querySelectorAll(".software-item");
+let postFilters = document.querySelectorAll(
+  ".post-container .post-listing.select"
+);
+console.log(postFilters);
+softwareAll.forEach((softwareSelect) => {
+  softwareSelect.addEventListener("click", () => {
+    softwareAll.forEach((newbtn) => newbtn.classList.remove("active"));
+    softwareSelect.classList.add("active");
+
+    //filter
+    const softwareValue = softwareSelect.textContent;
+    postFilters.forEach((postList) => {
+      postList.className = "";
+      postList.style.display = "none";
+      if (
+        postList.getAttribute("data-software-filter") ==
+          softwareValue.toLowerCase() ||
+        softwareValue == "All"
+      ) {
+        postList.className = "post-listing";
+        postList.style.display = "block";
+      }
+    });
+  });
+});
